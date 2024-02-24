0523b3ae0cd60a861859335c930e5076aa8e754f
Polkadot-Stingray <109863563+Polkadot-Stingray@users.noreply.github.com>
2024-02-24 20:19:32 +0900

    [Gold I] Title: K번째 수, Time: 1380 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/1300. K번째 수/K번째 수.py
@@ -0,0 +1,21 @@
+import sys
+input=sys.stdin.readline
+
+N=int(input())
+K=int(input())
+
+start=1
+end=K
+ans=0
+
+while start<=end:
+    mid=int((start+end)/2)
+    cnt=0
+    for i in range(1, N+1):
+        cnt+=min(int(mid/i), N)
+    if cnt<K:
+        start=mid+1
+    else:
+        ans=mid
+        end=mid-1
+print(ans)        
