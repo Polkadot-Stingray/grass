d01fe5780c5f9438c303cb97a47b127bbb22e52b
Polkadot-Stingray <109863563+Polkadot-Stingray@users.noreply.github.com>
2024-03-03 09:10:19 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.03 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -0,0 +1,24 @@
+import heapq
+def solution(operations):
+    answer = []
+    q = []
+    for i in operations:
+        x, num=i.split() 
+        num=int(num)
+        
+        if x=='I':
+            heapq.heappush(q, num)
+        elif x=='D' and num==1:
+            if len(q)!=0:
+                max_value=max(q)
+                q.remove(max_value)
+        else:
+            if len(q)!=0:
+                heapq.heappop(q)
+    
+    if len(q)==0:
+        answer=[0, 0]
+    else:
+        answer=[max(q), heapq.heappop(q)]
+        
+    return answer
